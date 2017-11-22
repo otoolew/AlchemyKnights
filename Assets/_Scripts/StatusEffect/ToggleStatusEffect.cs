64351df196a91f4d33a3c67a676500d58f30a8c4
otoolew <otoolew@gmail.com>
2017-11-22 17:05:06 -0500

    Worldbuilding

---
+++ Assets/_Scripts/StatusEffect/ToggleStatusEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleStatusEffect : StatusEffect {
+
+    public override void InitializeStatusEffect(GameObject obj)
+    {
+ 
+    }
+
+    public override void ApplyStatusEffect()
+    {
+        
+    }
+    public override void RemoveStatusEffect()
+    {
+        
+    }
+
+}
